076ec5f27e7ef67fc6730db19895740d6651892e
dramaticrobotic <57208077+dramaticrobotic@users.noreply.github.com>
2022-04-13 18:21:51 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Whirlwind Browser',
+    version='2.4',
+    packages=[''],
+    url='',
+    license='',
+    author='f1dg3t',
+    author_email='dramaticrobotic@icloud.com',
+    description='Whirlwind Web Browser from Captains Crew: The Animated Series'
+)
